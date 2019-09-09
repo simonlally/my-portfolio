eac57ed70e43364297ed54c7d301f4e340bfcfda
simonlally <simonlallydev@gmail.com>
2019-09-09 15:53:42 -0400

    base of MenuBar component done

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 export default function MenuBar() {
+  function handleClick() {}
+
   return (
     <Menu pointing secondary>
-      <Menu.Item name="Portfolio" />
-      <Menu.Item name="Resume" />
+      <Menu.Item name="Portfolio" as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="Resume" as={Link} to="/resume" />
+      </Menu.Menu>
     </Menu>
   );
 }
