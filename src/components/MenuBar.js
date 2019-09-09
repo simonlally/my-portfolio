550c17850ca1aa5c140f892baaa7bcb404c1aa60
Simon Lally <simonlallydev@gmail.com>
2019-09-09 13:15:59 -0400

    menubar component boilerplate

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,17 +1,12 @@
-import React, { useState}  from "react";
+import React, { useState } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 export default function MenuBar() {
-
-    const [isClicked] = useState(false);
-
-    const handleClick = (e) => 
-
   return (
     <Menu pointing secondary>
       <Menu.Item name="Portfolio" />
-      <Menu.Item name ="Resume"/>
+      <Menu.Item name="Resume" />
     </Menu>
   );
 }
