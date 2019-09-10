808d19d3698f412d2e136236242ab528afdafdfd
simonlally <simonlallydev@gmail.com>
2019-09-10 18:08:44 -0400

    image circle placeholder component

---
+++ src/components/ImageCircle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image } from "semantic-ui-react";
+
+const ImageCircle = () => (
+  <Image
+    src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+    size="medium"
+    circular
+  />
+);
+
+export default ImageCircle;
