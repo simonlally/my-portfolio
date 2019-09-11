362deb0b428becd844621bf0c695b5bdfbc02b6a
simonlally <simonlallydev@gmail.com>
2019-09-10 22:26:27 -0400

    mild refactor

--- src/components/CardProfile.js
+++ src/components/CardProfile.js
@@ -2,12 +2,15 @@ import React from "react";
 import ImageCircle from "../components/ImageCircle";
 import { Card, Container, Icon } from "semantic-ui-react";
 
+const imgSource =
+  "https://react.semantic-ui.com/images/wireframe/square-image.png";
+
 export default function CardProfile() {
   return (
     <Container textAlign="center">
       <Card fluid>
         <Card.Content>
-          <ImageCircle />
+          <ImageCircle imgSource={imgSource} />
           <Card.Header>Simon Lally</Card.Header>
           <Card.Meta>Full Stack Developer</Card.Meta>
           <Card.Description>I'm Hirable!</Card.Description>
--- src/components/ImageCircle.js
+++ src/components/ImageCircle.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Image } from "semantic-ui-react";
 
-const ImageCircle = () => (
-  <Image
-    src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-    size="medium"
-    circular
-  />
+const ImageCircle = ({ imgSource }) => (
+  <Image src={imgSource} size="medium" circular />
 );
 
 export default ImageCircle;
