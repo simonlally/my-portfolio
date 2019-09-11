1af3db1ab5b546716716eb3fb23efb89269da994
simonlally <simonlallydev@gmail.com>
2019-09-11 17:38:46 -0400

    styling and refactoring

--- src/components/CardProfile.js
+++ src/components/CardProfile.js
@@ -5,15 +5,15 @@ import { Card, Container, Icon } from "semantic-ui-react";
 const imgSource =
   "https://react.semantic-ui.com/images/wireframe/square-image.png";
 
-export default function CardProfile() {
+export default function CardProfile({ name, about, status }) {
   return (
     <Container textAlign="center">
       <Card fluid>
         <Card.Content>
           <ImageCircle imgSource={imgSource} />
-          <Card.Header>Simon Lally</Card.Header>
-          <Card.Meta>Full Stack Developer</Card.Meta>
-          <Card.Description>I'm Hirable!</Card.Description>
+          <Card.Header>{name}</Card.Header>
+          <Card.Meta>{about}</Card.Meta>
+          <Card.Description>{status}</Card.Description>
         </Card.Content>
         <Card.Content extra>
           <a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,27 @@
 import React from "react";
+
 import CardProfile from "../components/CardProfile";
 import ProjectCard from "../components/ProjectCard";
 
 export default function Home() {
   return (
     <div>
-      <CardProfile />
+      <CardProfile
+        name="Simon Lally"
+        about="Full Stack Developer"
+        status="I'm Hirable!"
+      />
       <ProjectCard
         name="Howler"
         desc="social media site built in: Mongo Express React Node GraphQL"
+      />
+      <ProjectCard
+        name="Package Management System"
+        desc="system design to track incoming packages for an appartment building or complex"
+      />
+      <ProjectCard
+        name="Combinations"
+        desc="coin combinations challenge, vue frontend rails backend"
       />
     </div>
   );
