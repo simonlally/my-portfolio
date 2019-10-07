f1bd789fa58b9716040cc2ecb1e28a919dd1f0ac
simonlally <simonlallydev@gmail.com>
2019-10-07 19:32:06 -0400

    styled project image overlay

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
-import { Card, Icon, Button, Image } from "semantic-ui-react";
+import { Card, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 import "semantic-ui-css/semantic.min.css";
 
 const imgSrc =
   "https://holoweb.net/~liam/pictures/r/1C/img_8179-path-to-beach-q100-500x200.jpg";
 
-export default function ProjectCard({ name, desc }) {
+export default function ProjectCard({
+  name,
+  desc,
+  frontEndTech,
+  backEndTech,
+  githubLink,
+  herokuLink
+}) {
   const [isHovered, setHovered] = useState(false);
   return (
     <Card
@@ -32,14 +40,45 @@ export default function ProjectCard({ name, desc }) {
               height: "100%",
               opacity: 0.8,
               textAlign: "center",
-              display: "flex",
+              display: "block",
               alignItems: "center",
               justifyContent: "center",
               width: "100%"
             }}
           >
-            {" "}
-            {desc}
+            <div style={{ paddingTop: "100px" }}>
+              <h1> {desc} </h1>
+            </div>
+            <div style={{ paddingTop: "20px" }}>
+              <h3> front end tech</h3>
+              <div>
+                {frontEndTech.map(x => (
+                  <Button
+                    style={{ marginLeft: "10px", marginRight: "10px" }}
+                    content={x}
+                  ></Button>
+                ))}
+              </div>
+            </div>
+            <div style={{ paddingTop: "10px" }}>
+              <h3> back end tech </h3>
+              <div>
+                {backEndTech.map(x => (
+                  <Button
+                    style={{ marginLeft: "10px", marginRight: "10px" }}
+                    content={x}
+                  ></Button>
+                ))}
+              </div>
+            </div>
+            <div style={{ margin: "20px" }}>
+              <Button component={Link} to={githubLink}>
+                View me on github
+              </Button>
+              <Button component={Link} to={herokuLink}>
+                View me live on Heroku
+              </Button>
+            </div>
           </div>
         </div>
       )}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,15 @@ import React from "react";
 import CardProfile from "../components/CardProfile";
 import ProjectCard from "../components/ProjectCard";
 
+const howlerFront = ["React", "Semantic-UI", "GraphQL"];
+const howlerBack = ["Node", "Express", "GraphQL", "Mongoose", "MongoDB"];
+
+const pkgFront = ["React", "Material-UI", "Firebase"];
+const pkgBack = ["Node", "Express", "Firebase"];
+
+const combFront = ["Vue"];
+const combBack = ["Ruby on Rails"];
+
 export default function Home() {
   return (
     <div>
@@ -14,14 +23,25 @@ export default function Home() {
       <ProjectCard
         name="Howler"
         desc="social media site built in: Mongo Express React Node GraphQL"
+        frontEndTech={howlerFront}
+        backEndTech={howlerBack}
+        githubLink={"https://github.com/simonlally/howler"}
+        herokuLink={"https://github.com/simonlally/mongo-graphql/"}
       />
       <ProjectCard
         name="Package Management System"
         desc="system design to track incoming packages for an appartment building or complex"
+        frontEndTech={pkgFront}
+        backEndTech={pkgBack}
+        githubLink={"https://github.com/simonlally/pkg-management-react"}
+        herokuLink={"https://github.com/simonlally/pkg-management-firebase"}
       />
       <ProjectCard
         name="Combinations"
         desc="coin combinations challenge, vue frontend rails backend"
+        frontEndTech={combFront}
+        backEndTech={combBack}
+        githubLink={"https://github.com/simonlally/combinations"}
       />
     </div>
   );
