cd66e6fc5c562a65ceb89b05b44d1f5d4804b2f6
simon lally <simonlallydev@gmail.com>
2019-12-11 21:05:31 -0800

    Testing .env file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
+import ReactGA from "react-ga";
 
 function App() {
   return (
